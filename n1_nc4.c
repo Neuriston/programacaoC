23870969571b1b1bed699b9f84d93e107a2bef7d
neuriston <134342490+Neuriston@users.noreply.github.com>
2025-04-12 01:57:00 +0000

    crie aula N2_Codar

---
+++ n1_nc4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+ int idade = 32;
+ int quantidade = 1;
+ float altura = 1,58;
+ double peso = 65,3;
+ char letra 'N'
+ char nome[20] = neuri;
+
+ printf("A idade do %s é: %d\n", nome, idade);
+ printf(" A altura é: %.2f\n", altura);
+ 
+  // sintaxe scanf
+  //scanf
+
+  printf("Digite a sua idade");
+  scanf("%d", &idade);
+
+  printf("Digite a sua altura");
+  scanf("%f", &altura);
+  printf("A altura é: %f\n", altura);
+  
+  
+
+}
